10a3de8a1a009f62c54868e44e92e3351e130aa9
zfc <zhuliderb@gmail.com>
2025-11-12 19:34:49 +0800

    update mx

--- swift_config/internvl3.5_1b_train_mx.sh
+++ swift_config/internvl3.5_1b_train_mx.sh
@@ -26,23 +26,23 @@ echo "Using port: $MASTER_PORT"
 nohup swift sft \
     --model '/root/data/model/InternVL3_5-1B'\
     --dataset '/root/data/dataset/VLM-formula-recognition-dataset_intern_camp/train/train_mini_abs.jsonl' \
-    --eval_steps 1000 \
+    --eval_steps 100 \
     --train_type lora \
     --lora_rank 16 \
     --lora_dropout 0.01 \
     --lora_alpha 32 \
     --torch_dtype bfloat16 \
-    --num_train_epochs 3 \
+    --num_train_epochs 1 \
     --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 2 \
     --learning_rate 5e-5 \
     --warmup_ratio 0.05 \
     --gradient_accumulation_steps 4 \
-    --save_steps 2000 \
-    --save_total_limit 10 \
+    --save_steps 25 \
+    --save_total_limit 3 \
     --gradient_checkpointing_kwargs '{"use_reentrant": false}' \
     --logging_steps 10 \
-    --max_length 4096 \
+    --max_length 8000 \
     --output_dir ./swift_output/SFT-InternVL3_5-1B\
     --dataset_num_proc 8 \
     --dataloader_num_workers 8 \
